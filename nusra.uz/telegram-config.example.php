5711cc35b952e61a503f614ee8e0d6556b2e27a9
nusratulloh0110-png <nusratulloh0110@gmail.com>
2026-05-23 16:26:05 +0500

    Delete nusra.uz directory

--- nusra.uz/telegram-config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    // Create telegram-config.php from this file on the server only.
-    // Regenerate any token that has previously been exposed in browser JavaScript.
-    'bot_token' => 'YOUR_NEW_BOT_TOKEN',
-    'chat_id' => 'YOUR_CHAT_ID',
-];
